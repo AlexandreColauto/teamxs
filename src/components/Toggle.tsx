c60d7d4bb46c2598430ba460b432392d3ed28b48
Alexandre Colauto <43041764+AlexandreColauto@users.noreply.github.com>
2022-04-12 04:33:04 +0100

    last_before_revision

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -14,37 +14,37 @@ export default function CustomLabelExample(props: callback) {
   };
   return (
     <Switch.Group>
-      <div className="flex items-center">
-        <Switch
-          checked={enabled}
-          onChange={setEnabled}
+    <div className="flex items-center">
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${
+          enabled ? "bg-[#F2F2F2]" : "bg-[#F2F2F2]"
+        } relative inline-flex items-center  h-14 rounded-full w-[150px] transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+      >
+        <span
           className={`${
-            enabled ? "bg-[#63add7]" : "bg-[#63add7]"
-          } relative inline-flex items-center  h-14 rounded-full w-[150px] transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
-        >
-          <span
-            className={`${
-              enabled
-                ? "translate-x-[5.6rem] w-[3.6rem]"
-                : "translate-x-1 w-[5.6rem]"
-            } inline-block  h-12 transform bg-white rounded-full transition-transform`}
-          />
-          <div className="absolute w-full">
-            <div className="flex justify-center">
-              <p
-                className={`${!enabled ? "text-[#708496]" : "text-white"} mr-4`}
-              >
-                Collection{" "}
-              </p>
-              <p
-                className={`${enabled ? "text-[#708496]" : "text-white"} mr-1`}
-              >
-                {" "}
-                NFT{" "}
-              </p>
-            </div>
+            enabled
+              ? "translate-x-[5.6rem] w-[3.6rem]"
+              : "translate-x-1 w-[5.6rem]"
+          } inline-block  h-12 transform bg-[#404D3A] rounded-full  transition-transform`}
+        />
+        <div className="absolute w-full">
+          <div className="flex justify-center">
+            <p
+              className={`${!enabled ? "text-[#E8C39C]" : "text-[#404D3A]"} mr-4`}
+            >
+              Collection{" "}
+            </p>
+            <p
+              className={`${enabled ? "text-[#E8C39C]" : "text-[#404D3A]"} mr-1`}
+            >
+              {" "}
+              NFT{" "}
+            </p>
           </div>
-        </Switch>
+        </div>
+      </Switch>
       </div>
     </Switch.Group>
   );
